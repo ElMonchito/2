9e69d33003592d6010a4515b4342bcec3821e262
DarkNovatec <alexiskry1234@gmail.com>
2022-11-19 11:22:50 -0500

    prueba 1

--- src/App.js
+++ src/App.js
@@ -1,58 +1,52 @@
-import { fnCrear, fnRead, fnUpdate, fnDelete } from './componente/api';
+import { collection, getDocs, query, doc, deleteDoc, where, onSnapshot} from "firebase/firestore";
 import { useEffect, useState } from "react";
+import AppForm from "./componente/AppForm";
+import {db} from "./componente/firebase";
 
-function App() {
-  const [nombre, setNombre] = useState(null);
-  const [codigo, setCodigo] = useState(null);
-  const [registro, setRegistro] = useState(null);
+function App() {  
+  ///////////////////////////////////////////////////////////////////////
+  ////////// READ - fnRead - LECTURA A BD ///////////////////////////////
+  ///////////////////////////////////////////////////////////////////////
+  const [idActual, setIdActual] = useState("");
+  const [docsBD, setDocsBD] = useState([]);
+  const [orden, setOrden] = useState(0);
+  const i=1;
+  //console.log(docsBD);
+useEffect (( )=> {
+  const xColeccionConQuery = query(collection(db, "persona"), where("nombre","!=",""));
+  const unsubscribe = onSnapshot(xColeccionConQuery, (xDatosBD)=>{
+    const xDoc =[];
+    xDatosBD.forEach((doc) =>{
+      xDoc.push({id: doc.id, ...doc.data()});
+    });
+    setDocsBD(xDoc);
+  });
+},[idActual]);
+
+  ///////////////////////////////////////////////////////////////////////
+  ////////// DELETE - fnDelete - Eliminar registros /////////////////////
+  ///////////////////////////////////////////////////////////////////////
   
-  
-  const appCrear = () => {
-    fnCrear(nombre);
-    appRead();
-  }
 
-  const appRead = async () => {
-    const p = await fnRead();
-    setRegistro(p.docs);
-    //console.log(p.docs[0].data());  
-  }
-
-  useEffect( () => {
-    appRead(); 
-  }, [registro])
-
-  const appUpdate = async () =>{
-    //console.log(codigo, nombre);
-    await fnUpdate(codigo, nombre);
-    appRead();
-  }
-
-  const  appDelete = async () => {
-    await fnDelete(codigo);
-    appRead();
-  }
+  const fnDelete = () => {
+    console.log("Se elimino...");
+  };
 
   return (
-    
-    <div className="App"  style={{background: "black", width:"350px", color:"cyan"}} >
-      <h1>TAREA 1</h1>
-      <input type="text" onChange={ e => setNombre(e.target.value)} 
-        placeholder="Nombres completos" /> 
-      <br></br>  
-      <input type="text" onChange={ e => setCodigo(e.target.value)} 
-        placeholder="Código de persona" /> 
-
-      <br></br>
-      <button onClick={appCrear} >Guardar</button>
-      <br></br>
-      <button onClick={appDelete}>Eliminar</button>
-      <br></br>
-      <button onClick={appUpdate}>Actualizar</button>
-
+    <div style={{background:"black", padding:"10px", color:"cyan"}}>
+      <h1>Formulario</h1>
+      <h3>READ / DELETE</h3>
+      <AppForm {...{idActual, setIdActual}} />
       {
-        registro && registro.map( p => <p key={p.id}>{p.id} - { p.data().name }</p>) 
-      }
+        docsBD.map((p) => 
+        <p key={p.id}> 
+        N.{i} - {p.nombre} ---
+        <button onClick={() => fnDelete(p.id)}>Eliminar</button>
+        ---
+        <button onClick={() => setIdActual(p.id)}>Actualizar</button>
+        <br></br>
+        </p> )
+      }      
     </div>
   );
 }
